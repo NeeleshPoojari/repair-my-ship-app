32cf1409e4c06cdf8acc88363af4422bdce70b7f
neepooja <neepooja@publicisgroupe.net>
2020-04-26 00:30:54 +0530

    Added Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import {BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home.component';
-import Register from './pages/Register.component';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component = {Home} ></Route>
-        <Route exact path='/register' component = {Register} ></Route>
-      </Switch>
-    </BrowserRouter>
-  );
+// theme
+import theme from './theme'
+// header
+import Header from './containers/Header'
+
+// pages
+import Home from './pages/Home'
+import Register from './pages/Register'
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/register" component={Register} />
+            </Switch>
+          </>
+        </BrowserRouter>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
